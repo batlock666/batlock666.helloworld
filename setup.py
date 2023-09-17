07f10b44372d938d934448a886d7f53412f3a9f7
Bert Vanderbauwhede <batlock666@gmail.com>
2023-09-17 10:34:05 +0200

    Add script setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# NOQA: D100
+
+from setuptools import setup
+
+setup(
+    name="batlock666.helloworld",
+    version="0.1.0",
+    description='A "Hello, World!" program.',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",  # NOQA: E501
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["batlock666", "helloworld"],
+    author="Bert Vanderbauwhede",
+    author_email="batlock666@gmail.com",
+    url="https://github.com/batlock666/batlock666.helloworld",
+    license="GPL-3.0-or-later",
+)
